45c4994decea8e14fdc11d94c5c969c3dc030fa2
indexphp <andi.uph@gmail.com>
2018-09-05 15:35:31 +0700

    Update bitcoinrpc.cpp

--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -42,7 +42,7 @@ void ThreadRPCServer3(void* parg);
 
 static inline unsigned short GetDefaultRPCPort()
 {
-    return GetBoolArg("-testnet", false) ? 33372 : 23372;
+    return GetBoolArg("-testnet", false) ? 23371 : 23373;
 }
 
 Object JSONRPCError(int code, const string& message)
